07252df15cac69351b3246a54eecf1a963c819bd
arloashton <143569631+arloashton@users.noreply.github.com>
2023-11-15 16:16:56 -0500

    polished formatting

--- RunMatrix.java
+++ RunMatrix.java
@@ -4,17 +4,19 @@ public class RunMatrix {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     Matrix matrix;
-
+    //REQUESTS USER SIZE (STEP 1)//
     System.out.println("What size should the matrix be?");
     int size = scan.nextInt();
-    
-    matrix = new Matrix(size);
 
+    matrix = new Matrix(size);
+    //0 MATRIX (STEP 2)//
     System.out.println("Matrix where all values are 0:");
     matrix.printMatrix();
+    //FILLED MATRIX (STEP 4)//
     matrix.fillMatrix();
     System.out.println("Matrix filled with given values:");
     matrix.printMatrix();
+    //FLIPPED MATRIX (STEP 6)//
     matrix.flip();
     System.out.println("Flipped Matrix:");
     matrix.printMatrix();
